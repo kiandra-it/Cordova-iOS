777793d9f707164716f2046caa920373d5ed6f00
dale.salter <dale.salter@kiandra.com.au>
2017-01-04 10:59:18 +1100

    FIXED: Accidental removal

--- addEmbedded.js
+++ addEmbedded.js
@@ -4,7 +4,7 @@ const xcode = require('xcode'),
     fs = require('fs'),
     path = require('path');
 
-module.exports = function( ) {
+module.exports = function(context) {
     if(process.length >=5 && process.argv[1].indexOf('cordova') == -1) {
         if(process.argv[4] != 'ios') {
             return; // plugin only meant to work for ios platform.
